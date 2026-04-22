d4bfea7874b8d512f2e7945d84e1077cc5613143
Zahary Karadjov <zahary@metacraft-labs.com>
2026-04-22 18:04:08 +0300

    fix: update for Nim trace writer (close + .ct format tests)

--- src/tracer.rs
+++ src/tracer.rs
@@ -460,6 +460,7 @@ impl AikenTracer {
         TraceWriter::finish_writing_trace_metadata(&mut *tracer.writer)
             .map_err(|e| eyre!("{e}"))?;
         TraceWriter::finish_writing_trace_paths(&mut *tracer.writer).map_err(|e| eyre!("{e}"))?;
+        tracer.writer.close().map_err(|e| eyre!("{e}"))?;
 
         Ok(())
     }
